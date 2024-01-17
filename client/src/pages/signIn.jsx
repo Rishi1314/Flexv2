b815fe25ca1fe7569be6fc835dfc6c9b8611108c
Rishi1314 <rishirajprof@gmail.com>
2024-01-17 20:19:03 +0530

    Updated upload image functionality

--- client/src/pages/signIn.jsx
+++ client/src/pages/signIn.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import {signInStart,signInFailure,signInSuccess} from "../redux/user/userSlice"
-import {useDispatch, useSelector} from 'react-redux'
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import OAuth from '../components/OAuth';
 
-export default function Signin() {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const {loading,error}=useSelector((state)=>state.user)
+  const { loading, error } = useSelector((state) => state.user);
+
   const navigate = useNavigate();
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -16,7 +21,7 @@ export default function Signin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      dispatch(signInStart())
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -26,49 +31,50 @@ export default function Signin() {
       });
       const data = await res.json();
       if (data.success === false) {
-        dispatch(signInFailure(data))
+        dispatch(signInFailure(data));
         return;
       }
-      dispatch(signInSuccess(data))
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      dispatch(signInFailure(error))
+      dispatch(signInFailure(error));
     }
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-    <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-      <input
-        type='email'
-        placeholder='Email'
-        id='email'
-        className='bg-slate-100 p-3 rounded-lg'
-        onChange={handleChange}
-      />
-      <input
-        type='password'
-        placeholder='Password'
-        id='password'
-        className='bg-slate-100 p-3 rounded-lg'
-        onChange={handleChange}
-      />
-      <button
-        disabled={loading}
-        className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
-      >
-        {loading ? 'Loading...' : 'Sign In'}
-      </button>
-      <OAuth/>
-    </form>
-    <div className='flex gap-2 mt-5'>
-      <p>{`Don't Have an account?`}</p>
-      <Link to='/sign-up'>
-        <span className='text-blue-500'>Sign up</span>
-      </Link>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='Email'
+          id='email'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          id='password'
+          className='bg-slate-100 p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+        <OAuth />
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+        <Link to='/sign-up'>
+          <span className='text-blue-500'>Sign up</span>
+        </Link>
+      </div>
+      <p className='text-red-700 mt-5'>
+        {error ? error.message || 'Something went wrong!' : ''}
+      </p>
     </div>
-    <p className='text-red-700 mt-5'>{error ?error.message||'Something went wrong!':''}</p>
-  </div>
-
-  )
+  );
 }
