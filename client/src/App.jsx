5c1b92cd5ee86627b2865b177d95cbd5ba35c123
Rishi1314 <rishirajprof@gmail.com>
2024-01-17 23:29:03 +0530

    Completed home and about pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { About } from "./pages/About"
+import Home from "./pages/Home"
+import About from "./pages/About"
 import Profile from "./pages/Profile"
 import Signup from "./pages/signUp"
 import Header from "./components/Header"
