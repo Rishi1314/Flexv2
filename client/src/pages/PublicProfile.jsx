90b07f5b6b450072cd045aab641a4b7ee9c3fb78
Rishi1314 <rishirajprof@gmail.com>
2024-02-03 20:59:53 +0530

    Redirect user to his own page

--- client/src/pages/PublicProfile.jsx
+++ client/src/pages/PublicProfile.jsx
@@ -1,15 +1,18 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { FaGithub, FaLink } from 'react-icons/fa6';
-import { Link, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
 
 
 export const PublicProfile = () => {
+    const { currentUser } = useSelector((state) => state.user);
+
     const [user, setUser] = useState(null)
     const { id } = useParams();
     const techColors = { ReactJS: "82CD47", ExpressJS: "3C3633", Nodejs: "527853", MongoDB: "3A4D39", Python: "FFE382", Javascript: "FFA33C", TailwindCSS: "40A2D8", Flutter: "36BAF6", HTML: 'E5532D', CSS: "2D53E5", C: '085D9F', Cpp: '085D9F', MySQL: "F29418", Firebase:"F5831E" }
     const tagColors = { AIAficionado:"424769",CompetitiveCoder:"005B41",FullStackMagician:"7D7C7C",PythonPro:"FFE382",UIUXUnicorn:"711DB0",AppDeveloper:"0079FF",WebDeveloper: "00A9FF", Creator: "FD8D14", ReactRockstar: "82CD47" }
-
+    const navigate=useNavigate()
     const customConfig = {
         headers: {
         'Content-Type': 'application/json'
@@ -27,32 +30,34 @@ export const PublicProfile = () => {
     
       
     }, [id])
-    
+    if (id === currentUser.username) {
+        navigate("/profile")
+    }
   
     if (user) {
         return(<div className='h-[93vh] overflow-y-auto
         
         bg-[#141619] w-[100%] flex flex-col  items-center p-5 gap-2'>
             <div className='flex max-[767px]:flex-col gap-2 w-[100%]'>
-                <div className='h-[350px] shadow-lg ring-1 ring-white/5  gap-2 p-4 flex-col rounded-[30px] bg-[#1E1F24] max-[767px]:justify-between max-[767px]:w-[100%] flex items-center justify-center     w-[30%]'>
-                    <div className='  flex flex-col gap-2 items-center'>
-                        <img src={user.profilePicture} className='  border-[2px] w-[8rem] rounded-full aspect-square object-cover' alt="" />
-                        <div className='text-white flex flex-col'>
-                            <span className='max-[767px]:text-[150%] text-3xl underline'>{user.username}</span>
-                            <div className='flex justify-evenly'><span className='max-[767px]:text-[100%] text-xl'>{user.firstName}</span>
-                                <span className='text-xl max-[767px]:text-[100%] '>{user.lastName}</span></div>
-
-                        </div>
-                    </div>
-                    <div className='max-[767px]:w-[100%] max-[767px]:min-h-fit bg-white/50 w-[90%]  p-2 rounded-md'>
+            <div className="group before:hover:scale-95 before:hover:w-[99%] before:hover:h-[350px] before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-[100%] before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-orange-200 to-orange-700 before:absolute before:top-0  h-[350px] relative bg-[#1E1F24] flex items-center justify-center flex-col rounded-[30px] overflow-hidden w-[30%]  max-[767px]:w-[100%]">
+                    <div className="w-28 h-28 bg-blue-700 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-60 group-hover:-translate-x-0  group-hover:-translate-y-10 transition-all duration-500"><img src={user.profilePicture} className='  border-[2px] w-[8rem] rounded-full aspect-square object-cover' alt="" /></div>
+                    
+  <div className="z-10 w-[90%] group-hover:-translate-y-10 transition-all flex flex-col gap-2 items-center duration-500">
+                        <span className=" text-white text-2xl font-semibold">{user.username }</span>
+                        <div className='flex justify-evenly text-white w-[100%]'><span className='max-[767px]:text-[100%] text-xl'>{user.firstName}</span>
+                            <span className='text-xl max-[767px]:text-[100%] '>{user.lastName}</span></div>
+                            <div className='max-[767px]:w-[100%] max-[767px]:min-h-fit bg-white/50 w-[90%]  p-2 rounded-md'>
                         {`" ${user.description}. "`}
-                    </div>
-                    <Link to={"/editprofile"}>
+                        </div>
+                        {/* <Link to={"/editprofile"}>
                         <button className=' max-[767px]:text-[80%] bg-black hover:opacity-80
                   shadow-[0px_4px_16px_rgba(44, 44, 44, 1),_0px_8px_24px_rgba(44, 44, 44, 1),_0px_16px_56px_rgba(44, 44, 44, 1)] text-white py-2 px-4 rounded-xl'>
                             Edit Profile</button>
-                    </Link>
-                </div>
+                    </Link> */}
+                    </div>
+                    
+  
+</div>
                 <div className='shadow-lg ring-1 ring-white/5 p-4 rounded-[30px] bg-[#1E1F24] flex-col max-[767px]:w-[100%] flex h-[350px]  w-[70%] gap-2'>
 
 
