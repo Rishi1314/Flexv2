a77f4f5f3a93b261857942c1d87f0a1584250ec9
Rishi1314 <rishiraj13052003@gmail.com>
2024-02-10 13:39:04 +0530

    Project Page minor changes.

--- client/src/pages/EditProject.jsx
+++ client/src/pages/EditProject.jsx
@@ -25,6 +25,7 @@ export default function EditProject() {
     const [imageError, setImageError] = useState(false)
     const [imagePercentage, setImagePercentage] = useState(0)
     const { currentUser, loading, error } = useSelector((state) => state.user);
+    const techColors = { ReactJS: "82CD47", ExpressJS: "3C3633", Nodejs: "527853", NodeJS: "527853",MongoDB: "3A4D39", Python: "FFE382", Javascript: "FFA33C", TailwindCSS: "40A2D8", Flutter: "36BAF6", HTML: 'E5532D', CSS: "2D53E5", C: '085D9F', Cpp: '085D9F', MySQL: "F29418", Firebase: "F5831E" }
     useEffect(() => {
         if (!projectData) {
             const project = (currentUser.projects).find(o => o._id === params.projectId);
@@ -116,20 +117,21 @@ export default function EditProject() {
         return (
             <div className='h-[93vh] overflow-y-auto
         
-    bg-[#1E1F24] w-[100%] flex flex-col  p-5 gap-2'>
+    bg-[#1E1F24] w-[100%] flex flex-col justify-center items-center  gap-2'>
 
-            <div className='bg-[#141619] p-6 h-[90vh] rounded-2xl'>
+            <div className='bg-[#141619]  h-[90vh] overflow-y-hidden  w-[98%] rounded-2xl p-2 '>
 
-                <form onSubmit={handleSubmit} className='flex max-[767px]:flex-col max-[767px]:gap-1 gap-4 '>
-                    <div className='flex min-[768px]:bg-[#24252a8c] rounded-2xl flex-col w-1/2 max-[767px]:items-center max-[767px]:w-[100%] gap-2
-                    justify-center items-center '>
+                <form onSubmit={handleSubmit} className='flex h-[90vh] overflow-y-auto max-[767px]:flex-col max-[767px]:gap-1 gap-4 justify-center p-2 items-center '>
+                    <div className='flex min-[768px]:bg-[#24252a8c] rounded-2xl flex-col w-[48%] max-[767px]:items-center max-[767px]:w-[100%] gap-2 p-4
+                    justify-center  items-center '>
                         <div className='flex max-[767px]:flex-col gap-2 items-center'>
                             <input type='file' ref={fileRef} hidden accept='image/*' onChange={(e) => setImage(e.target.files[0])} />
                             <img
                                 src={formData.profilePicture || projectData.projectPicture}
                                 alt='profile'
-                                className=' w-[300px] h-[200px]  
-                                cursor-pointer rounded-md object-cover mt-2 '
+                                    className=' w-[300px] h-[200px]
+                                max-[767px]:w-[150px] max-[767px]:h-[100px] 
+                                cursor-pointer rounded-md object-cover'
                                 onClick={() => fileRef.current.click()}
                             />
                            
@@ -153,7 +155,15 @@ export default function EditProject() {
                             </div>
 
                         </div>
-                        <button className='bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition    p-3 uppercase hover:opacity-95 max-[767px]:w-[80%] disabled:opacity-80 w-[50%] ' >{loading ? "Loading.." : "Update"}</button>
+                            <button className='bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition    p-3 uppercase hover:opacity-95 max-[767px]:w-[80%] disabled:opacity-80 w-[50%] ' >{loading ? "Loading.." : "Update"}</button>
+                            <div className=' max-w-[50%] max-[767px]:max-w-[80%] w-fit flex bg-[#2f3035] p-3 rounded-xl gap-2 justify-center flex-wrap'>
+                                {
+                                    (projectData.techStack).map((tech) => {
+                                        let tagCol = tech.split(" ").join("")
+                                        return <span style={{ backgroundColor: `#${techColors[tagCol]}` }} className={` w-fit h-fit p-2 rounded-2xl text-white`} key={tech}>{tech}</span>
+                                    })
+                                    }
+                            </div>
                         <div className=" bg-[#ff5b5b] font-mukta rounded-lg border-white border-2 hover:bg-red-600 duration-200 max-[767px]:w-[80%] text-center cursor-pointer p-2">
                             <span className='text-white' onClick={handleDeleteAccout}>Delete Project</span>
                         </div>
@@ -162,7 +172,7 @@ export default function EditProject() {
                             {updateSuccess && 'User is updated successfully!'}
                         </p>
                     </div>
-                    <div className='w-[100%] flex items-center gap-2 flex-col'>
+                    <div className='w-[48%] max-[767px]:w-[95%] flex items-center gap-2 flex-col'>
                         <div className='w-[100%] flex justify-center'>
 
                             <input
@@ -195,7 +205,8 @@ export default function EditProject() {
                             <textarea id="description" onChange={handleChange} type='text'
                                 defaultValue={projectData.description}
                                 className='  h-[5em] bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition w-[50%] max-[767px]:w-[100%] resize-none text-wrap' maxLength={500} placeholder='Describe project in 100 characters.' />
-                        </div>
+                            </div>
+                           
                     </div>
 
 
